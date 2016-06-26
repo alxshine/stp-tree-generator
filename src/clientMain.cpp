ab94843b3c5e5e462cebd759a664414c4ff94f2b
alxshine <alexander.schloegl@student.uibk.ac.at>
2016-06-26 23:36:04 +0200

    added config files, parameters named like the variables

--- src/clientMain.cpp
+++ src/clientMain.cpp
@@ -10,6 +10,36 @@ int main(int argc, char **args){
     int port = 80;
     bool noConnect = false;
 
+    ifstream configRead("client.conf", ios_base::in);
+    try{
+        while(!configRead.eof()){
+            string line;
+            configRead >> line;
+            if(line.length() == 0)
+                break;
+            size_t colonLoc = line.find(":", 0);
+            string parameter, value;
+            parameter = line.substr(0, colonLoc);
+            value = line.substr(colonLoc+1);
+            transform(parameter.begin(), parameter.end(), parameter.begin(), ::tolower);
+
+            if(parameter == "port")
+                port = stoi(value);
+            else if(parameter == "outputfilename")
+                outputFileName = value;
+            else if(parameter == "inputfilename")
+                inputFileName = value;
+            else if(parameter == "hostname")
+                hostname = value;
+            else if(parameter == "noconnect"){
+                transform(value.begin(), value.end(), value.begin(), ::tolower);
+                noConnect = value == "true";
+            }
+        }
+    }catch(int i){
+        cerr << "Error on reading config file";
+    }
+
     for(int c = 1; c<argc; c++){
         auto param = string(args[c]);
         if(param == "-if")
@@ -24,6 +54,19 @@ int main(int argc, char **args){
             hostname = args[++c];
         
     }
+    if(!configRead.is_open()){
+        ofstream configWrite;
+        configWrite.open("client.conf", ios_base::out);
+        configWrite << "hostname:" << hostname << endl;
+        configWrite << "port:" << port << endl;
+        configWrite << "outputFileName:" << outputFileName << endl;
+        if(inputFileName.length() > 0)
+            configWrite << "inputFileName:" << inputFileName << endl;
+        configWrite << "noConnect:" << noConnect << endl;
+
+        configWrite.close();
+    }
+
     try{
         Sniffer s(noConnect, outputFileName, hostname, port);
         s.start(inputFileName);
--- src/serverMain.cpp
+++ src/serverMain.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
 
 #include "../inc/Server.hpp"
 
@@ -10,7 +15,31 @@ int main(int argc, char **args){
         string outputFileName = "server.log";
         bool createPidFile = true;
         int timeout = 10;
-        
+
+        ifstream configRead("server.conf", ios_base::in);
+        try{
+            while(!configRead.eof()){
+                string line;
+                configRead >> line;
+                if(line.length() == 0)
+                    break;
+                size_t colonLoc = line.find(":", 0);
+                string parameter, value;
+                parameter = line.substr(0, colonLoc);
+                value = line.substr(colonLoc+1);
+                if(parameter == "port")
+                    port = stoi(value);
+                else if(parameter == "outputFileName")
+                        outputFileName = value;
+                else if(parameter == "createPidFile"){
+                    transform(value.begin(), value.end(), value.begin(), ::tolower);
+                    createPidFile = value == "true";
+                }
+            }
+        }catch(int a){
+            cerr << "Error on reading config file";
+        }
+
         for(int c = 1; c<argc; c++){
             auto param = string(args[c]);
             if(param == "-p")
@@ -22,7 +51,18 @@ int main(int argc, char **args){
             else if(param == "-t")
                 timeout = atoi(args[++c]);
         }
+ 
+        if(!configRead.is_open()){
+            ofstream configWrite;
+            configWrite.open("server.conf", ios_base::out);
+            configWrite << "port:" << port << endl;
+            configWrite << "outputFileName:" << outputFileName << endl;
+            configWrite << "timeout:" << timeout << endl;
+
+            configWrite.close();
+        }        
         Server serv(port, outputFileName, createPidFile, timeout);
+        
         serv.run();
     }catch(char const *c){
         cout << c << endl;
