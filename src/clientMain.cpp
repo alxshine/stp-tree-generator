22825b530b10d2c6eeaa7b89f65db61e0de20a9d
alxshine <alexander.schloegl@student.uibk.ac.at>
2016-08-19 21:09:47 +0200

    added interface output

--- src/clientMain.cpp
+++ src/clientMain.cpp
@@ -72,6 +72,7 @@ int main(int argc, char **args){
 
     try{
         Sniffer s(noConnect, outputFileName, hostname, port);
+        cout << "starting Sniffer on device: " << deviceName << endl;
         s.start(inputFileName, deviceName);
     }catch (char const *c){
         cerr << c << endl;
